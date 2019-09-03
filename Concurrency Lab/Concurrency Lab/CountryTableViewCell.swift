9c77c3708dbd9ccedc933a6c6f96aca982fc3dfc
Jason Ruan <jasonruan@pursuit.org>
2019-09-03 14:37:19 -0400

    Adds outlets to table view cell cocoa touch file

--- Concurrency Lab/Concurrency Lab/CountryTableViewCell.swift
+++ Concurrency Lab/Concurrency Lab/CountryTableViewCell.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class CountryTableViewCell: UITableViewCell {
-
+    
+    // MARK: -- IBOutlets
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var capitalLabel: UILabel!
+    @IBOutlet weak var populationLabel: UILabel!
+    @IBOutlet weak var flagImageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
