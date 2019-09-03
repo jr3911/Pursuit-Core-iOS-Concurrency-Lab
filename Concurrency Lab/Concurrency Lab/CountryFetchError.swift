67ad83c2cbab667a2796f7e562268f59782cd483
Jason Ruan <jasonruan@pursuit.org>
2019-09-03 16:49:20 -0400

    Adds two new swift files to deal with fetching and fetch errors

---
+++ Concurrency Lab/Concurrency Lab/CountryFetchError.swift
@@ -0,0 +1,11 @@
+//
+//  CountryFetchErrors.swift
+//  Concurrency Lab
+//
+//  Created by Jason Ruan on 9/3/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import Foundation
+
+struct CountryFetchError
---
+++ Concurrency Lab/Concurrency Lab/CountryFetchingService.swift
@@ -0,0 +1,9 @@
+//
+//  CountryFetchingService.swift
+//  Concurrency Lab
+//
+//  Created by Jason Ruan on 9/3/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import Foundation
