01814880084e5b881b2f0bd4b750f699888ff63d
Jason Ruan <jasonruan@pursuit.org>
2019-09-03 14:27:54 -0400

    Adds cocoa touch file for prototype cell and links to corresponding cell in main.storyboard

---
+++ Concurrency Lab/Concurrency Lab/CountryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CountryTableViewCell.swift
+//  Concurrency Lab
+//
+//  Created by Jason Ruan on 9/3/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class CountryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
