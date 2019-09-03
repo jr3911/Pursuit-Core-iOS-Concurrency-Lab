2849c4ce1bb61973df06cc5cbb99d0c2393f8685
Jason Ruan <jasonruan@pursuit.org>
2019-09-03 16:51:12 -0400

    Defines enum for possible errors when fetching data from url

--- Concurrency Lab/Concurrency Lab/CountryFetchError.swift
+++ Concurrency Lab/Concurrency Lab/CountryFetchError.swift
@@ -8,4 +8,26 @@
 
 import Foundation
 
-struct CountryFetchError
+enum CountryFetchError: Error, CustomStringConvertible {
+    case badURL
+    case responseError(Error)
+    case noURLResponse
+    case noData
+    case badURLResponse(Int)
+    
+    var description: String {
+        switch self {
+        case .badURL:
+            return "Invalid URL"
+        case let .responseError(error):
+            return "Response Error: \(error)"
+        case.noURLResponse:
+            return "No URL response"
+        case .noData:
+            return "No data received"
+        case let .badURLResponse(statusCode):
+            return "URL Response error: \(statusCode)"
+        }
+    }
+}
+
